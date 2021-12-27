7af2f22c641e79fede94418eaf6ff16b1b687b27
LinuXthusiast <96730918+Xthusiast@users.noreply.github.com>
2021-12-27 17:23:32 +0200

    Add files via upload

---
+++ Apt Updater.sh
@@ -0,0 +1,10 @@
+figlet "Welcome to system updater"
+
+read -r -p "Do you want to update your system? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    sudo apt -qq update && sudo apt upgrade
+    figlet "Done"
+else
+    echo "Ok"
+fi
---
+++ Flatpak Updater.sh
@@ -0,0 +1,10 @@
+figlet "Welcome to flatpak updater"
+
+read -r -p "Do you want to update your flatpak apps? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    sudo flatpak update -y
+    figlet "Done" 
+else
+    echo "Ok"
+fi
