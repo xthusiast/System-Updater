3f02f671f596df4766f67df1f5c703fe92b2ebe8
LinuXthusiast <96730918+Xthusiast@users.noreply.github.com>
2021-12-30 13:53:47 +0200

    Update Apt Updater.sh

--- Apt Updater.sh
+++ Apt Updater.sh
@@ -1,9 +1,9 @@
-figlet "Welcome to system updater"
+figlet "Welcome to system updater!"
 
 read -r -p "Do you want to update your system? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-    sudo apt -qq update && sudo apt upgrade
+    sudo apt -qq update && sudo apt upgrade -y
     figlet "Done"
 else
     echo "Ok"
